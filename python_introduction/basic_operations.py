ddeffaf23b8513f95194378f5457945e5267427f
Josphat <josphatkkioko@gmail.com>
2025-07-12 06:16:22 +0300

    The basic operations python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# defining variables
+
+number1 = 10
+number2 = 5
+
+#performing arithmetic operations
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# printing the results
+
+print ( "Addition",  number1, "and", number2, "is", addition )
+print ("Subtraction of", number1, "and", number2, "is", subtraction )
+print ("Multiplication of", number1, "and", number2, "is", multiplication)
