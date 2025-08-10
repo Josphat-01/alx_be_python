3b82614c1ef41b7451a62288d9f4e74246f7dd3c
Josphat <josphatkkioko@gmail.com>
2025-08-10 19:52:12 +0300

    update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 
 def display_menu():
-    print("\n--- Shopping List Manager ---")
+    print("Shopping List Manager ---")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
@@ -36,7 +36,6 @@ def view_list(shopping_list):
             print(f"{idx}. {item}")
     else:
         print("\nYour shopping list is empty.")
-
 
 def main():
 
