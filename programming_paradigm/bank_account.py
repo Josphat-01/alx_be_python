e2a510dd988c89ad4663b92212cfda575d15d9cf
Josphat <josphatkkioko@gmail.com>
2025-09-01 14:48:11 +0300

    new coomit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -22,4 +22,4 @@ class BankAccount:
 
     def display_balance(self):
         """Return a formatted string with the current balance."""
-        return (f"Current Balance: ${self.__account_balance:.2f}")
+        return f"Current Balance: ${self.__account_balance:.2f}"
